55fd553ccd7f7d2773347a8299fa12a0d67e1f63
marcod08 <you@example.com>
2023-12-28 18:18:15 +0100

    D4 completato

--- D4.js
+++ D4.js
@@ -2,7 +2,11 @@
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function area(l1, l2) {
+    return l1 * l2;
+}
+
+console.log(area(3, 4))
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -10,21 +14,49 @@
  la loro somma moltiplicata per tre.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazySum(a, b) {
+    let somma;
+    if (a === b) {
+        somma = (a + b) * 3;
+    } else {
+        somma = a + b;
+    }
+    return somma;
+}
+
+console.log(crazySum(2, 3))
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(a, b = 19) {
+    let diff;
+    if (a > 19) {
+        diff = (a - b) * 3;
+    } else {
+        diff = a - b;
+    }
+    return diff;
+}
+
+console.log(crazyDiff(5))
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function boundary(n) {
+    if (!isNaN(n) && n >= 20 && n <= 100 || n === 400) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(boundary(400))
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -32,37 +64,82 @@
  ritornare la stringa originale senza alterarla.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function epify(string) {
+    if (typeof string === "string") {
+        if (!string.startsWith("EPICODE")) {
+            return "EPICODE " + string;
+        } else {
+            return string;
+        }
+    } else {
+        return "non è una stringa";
+    }
+}
+
+console.log(epify("EPICODE"));
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(n) {
+    if (n > 0 && n % 3 === 0 || n % 7 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(check3and7(9))
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(string) {
+    return string.split("").reverse().join("");
+}
+
+console.log(reverseString("ocraM"))
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function upperFirst(string) {
+    return string.replace(/\b\w/g, (a) => {
+        return a.toUpperCase();
+    });
+}
+
+console.log(upperFirst("ciao a tutti"));
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function cutString(string) {
+    return string.slice(1,-1);
+}
+
+console.log(cutString("ciao belli"))
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function giveMeRandom(n) {
+    let randomArray = []
+    if (!isNaN(n)) {
+        for (let i = 0; i < n; i++){
+            let randomNumber = Math.floor(Math.random()* 11);
+            randomArray.push(randomNumber)[i];
+        }
+    }
+    return randomArray;
+}
+
+console.log(giveMeRandom(6))
